cd94a7049cd420e4ed74ba3d65b36f58c05ffbb9
José Hernández <edwar.herascen2012@gmail.com>
2019-09-08 09:16:34 -0600

    Se agregaron los siguientes tests a la clase MyListTest: verifyNoUnexpectedInteractions, verifyOrdenOfInteractions, verifyInteractionNotOcurred y verifyMinimumMaximunOfTimes

--- src/test/java/ues/occ/edu/sv/ingenieria/prn335/junitmockitotests/MyListTest.java
+++ src/test/java/ues/occ/edu/sv/ingenieria/prn335/junitmockitotests/MyListTest.java
@@ -4,11 +4,10 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.*;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 /**/
 @ExtendWith(MockitoExtension.class)
@@ -36,12 +35,54 @@ public class MyListTest {
        public void verifyNoInteractionsWholeMock(){
            List<String> mockedList= mock(MyList.class);
            verifyZeroInteractions(mockedList);
-       }*/
+       }
     @Test
     public void verifyNoInteractionsWithMethod(){
          List<String> mockedList= mock(MyList.class);
          verify(mockedList, times(0)).size();
     }
     
+     @Test 
+     public void verifyNoUnexpectedInteractions(){
+         List<String> mockedList = mock(MyList.class);
+         mockedList.size();
+         mockedList.clear();
+         verify(mockedList).size();
+         verifyNoMoreInteractions(mockedList);
+     }
+
+    @Test 
+    public void verifyOrderOfInteractions(){
+        List<String> mockedList = mock(MyList.class);
+        mockedList.size();
+        mockedList.add("aString");
+        mockedList.clear();
+        
+        InOrder inOrder = Mockito.inOrder(mockedList);
+        inOrder.verify(mockedList).size();
+        inOrder.verify(mockedList).add("aString");
+        inOrder.verify(mockedList).clear();
+       
+       
+        
+    }
+  
+    @Test 
+    public void verifyInteractionNotOcurred(){
+        List<String> mockedList = mock(MyList.class);
+        mockedList.size();
+        verify(mockedList, never()).clear();
+    }
+ */
+    @Test
+    public void verifyMinumumMaximumOfTimes(){
+    List<String> mockedList = mock(MyList.class);
+    mockedList.clear();
+    mockedList.clear();
+    mockedList.clear();
     
+    verify(mockedList, atMost(5)).clear();
+    verify(mockedList, atLeast(1)).clear();
+    
+    }
 }
